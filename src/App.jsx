835f2857c56962bc4df12c08bba31bb7e208d2ea
M-Ari-ZM <azm8216@gmail.com>
2026-03-24 22:58:02 +0700

    slighty improve performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import WelcomePage from "./pages/Welcome";
-import Dashboard from "./pages/Dashboard";
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
-      <Route path="/home" element={<Dashboard />} />
+      <Route
+        path="/home"
+        element={
+          <Suspense fallback={<p>Loading...</p>}>
+            <Dashboard />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
