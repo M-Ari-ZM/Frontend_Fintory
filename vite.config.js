8fc166941bc6d5d1b291e7fffea64c35452d9640
M-Ari-ZM <azm8216@gmail.com>
2026-04-10 17:58:54 +0700

    updates package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Frontend_Fintory/",
+  base:
+    process.env.VITE_DEPLOY_TARGET === "gh-pages" ? "/Frontend_Fintory/" : "/",
 });
